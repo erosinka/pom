8a51e4485201f3ca34f29be6fceeca9ecd5f7c40
Elena Rosinskaya <erosinskaya@MacBook-Pro-Elena.local>
2016-06-12 19:28:12 +0200

    set up test. done

--- test/constructor.cc
+++ test/constructor.cc
@@ -1,4 +1,15 @@
+#define BOOST_TEST_MODULE first
+#include <boost/test/included/unit_test.hpp>
 
-int main () {
-    return 0;
+#include "rectangle.h"
+
+BOOST_AUTO_TEST_CASE(first)
+{
+    Rectangle obj;
+    BOOST_CHECK_EQUAL(obj.visible, false);
+    BOOST_CHECK_EQUAL(obj.xmin, 0);
+    BOOST_CHECK_EQUAL(obj.ymin, 0);
+    BOOST_CHECK_EQUAL(obj.xmax, 0);
+    BOOST_CHECK_EQUAL(obj.ymax, 0);
 }
+
